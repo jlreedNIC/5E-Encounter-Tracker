cd5ec7ff2b6bd5fc88cacd3902cfafba2494e70e
Jordan Reed <jlreed@students.nic.edu>
2020-11-03 23:04:44 -0800

    Created a window, a title, and 2 unclickable buttons. Need better way to organize graphics in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,61 @@
 #include "initiativelist.h"
 #include <iostream>
 #include "diceroll.h"
+#include "graphicsfunctions.h"
 #include <SFML/Graphics.hpp>
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800,800), "D&D 5E Encounter Tracker");
+    window.setPosition(sf::Vector2i(10, 30));
 
+    //load fonts
+    sf::Font fontHeader;
+    if(!fontHeader.loadFromFile("Fonts/Sketch-Gothic-School.ttf"))
+    {
+        //error
+    }
+    sf::Font fontButton;
+    if(!fontButton.loadFromFile("Fonts/Roboto-Thin.ttf"))
+    {
+        //error
+    }
+    //create header
+    sf::Text textHeader;
+    textHeader.setFont(fontHeader);
+    textHeader.setString("Initiative Tracker");
+    textHeader.setFillColor(sf::Color::Black);
+    textHeader.setCharacterSize(80);
+    textHeader.setPosition(120,50);
+
+    //creating buttons on main menu
+    sf::Text newButtonText;
+    newButtonText.setFont(fontButton);
+    newButtonText.setString("New Initiative");
+    newButtonText.setFillColor(sf::Color::White);
+    newButtonText.setCharacterSize(25);
+    newButtonText.setStyle(sf::Text::Bold);
+    newButtonText.setPosition(300, 315);
+
+    sf::RectangleShape newButton(sf::Vector2f(210.f, 60.f));
+    newButton.setFillColor(sf::Color::Black);
+    newButton.setOutlineThickness(2.f);
+    newButton.setOutlineColor(sf::Color::Red);
+    newButton.setPosition(275, 300);
+
+    sf::Text existingButtonText;
+    existingButtonText.setFont(fontButton);
+    existingButtonText.setString("Existing Initiative");
+    existingButtonText.setFillColor(sf::Color::White);
+    existingButtonText.setCharacterSize(25);
+    existingButtonText.setStyle(sf::Text::Bold);
+    existingButtonText.setPosition(280, 415);
+
+    sf::RectangleShape existingButton(sf::Vector2f(210.f, 60.f));
+    existingButton.setFillColor(sf::Color::Black);
+    existingButton.setPosition(275, 400);
+    existingButton.setOutlineThickness(2.f);
+    existingButton.setOutlineColor(sf::Color::Red);
 
     //event loop
     while(window.isOpen())
@@ -19,6 +68,15 @@ int main()
                 window.close();
             }
         }
+
+        //draw
+        window.clear(sf::Color::White);
+        window.draw(textHeader);
+        window.draw(newButton);
+        window.draw(newButtonText);
+        window.draw(existingButton);
+        window.draw(existingButtonText);
+        window.display();
     }
     return 0;
 }
