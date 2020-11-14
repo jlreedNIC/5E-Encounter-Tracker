8e200aa8f35d9979309e2bcb1e336e346a32efb0
Jordan Reed <jlreed@students.nic.edu>
2020-11-13 19:58:16 -0800

    Implemented textBoxSize function

--- textbox.cpp
+++ textbox.cpp
@@ -2,27 +2,16 @@
 
 TextBox::TextBox() : Button()
 {
-    if(!font.loadFromFile("Fonts/Roboto-Thin.ttf"))
-    {
-        //error loading file
-    }
-    text.setFont(font);
-    text.setString("");
     text.setCharacterSize(20);
-    text.setFillColor(sf::Color::Black);
+    text.setStyle(0);
 
 }
 
-TextBox::TextBox(const std::string &tString) : Button()
+TextBox::TextBox(const std::string &tString) : Button(tString)
 {
-    if(!font.loadFromFile("Fonts/Roboto-Thin.ttf"))
-    {
-        //error loading file
-    }
-    text.setFont(font);
-    text.setString(tString);
     text.setCharacterSize(20);
-    text.setFillColor(sf::Color::Black);
+    text.setStyle(0);
+    setTextBoxSize(sf::Vector2f(0.f,0.f));
 }
 
 TextBox::~TextBox()
@@ -32,7 +21,12 @@ TextBox::~TextBox()
 
 void TextBox::setTextBoxSize(const sf::Vector2f &size)
 {
-
+    //either max of text size or parameter size
+    sf::FloatRect textBound = text.getGlobalBounds();
+    sf::Vector2f textSize = {textBound.width + 10, textBound.height + 10};
+    textSize.x = std::max(textSize.x, size.x);
+    textSize.y = std::max(textSize.y, size.y);
+    setSize(textSize);
 }
 
 void TextBox::setTextBoxPosition(const sf::Vector2f &size)
