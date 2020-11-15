38e1d6b7966a69f02d4e5788a4e7ff5f65fb513e
Jordan Reed <jlreed@students.nic.edu>
2020-11-15 10:51:11 -0800

    Implemented textBoxPosition functions. Changed texture of text box.

--- textbox.cpp
+++ textbox.cpp
@@ -4,7 +4,13 @@ TextBox::TextBox() : Button()
 {
     text.setCharacterSize(20);
     text.setStyle(0);
-
+    setTextBoxSize(sf::Vector2f(0.f, 0.f));
+    setTextPositionLeft();
+    texture.loadFromFile(nullptr);
+    setTexture(nullptr);
+    setFillColor(sf::Color::White);
+    setOutlineColor(sf::Color::Black);
+    setOutlineThickness(2);
 }
 
 TextBox::TextBox(const std::string &tString) : Button(tString)
@@ -12,6 +18,11 @@ TextBox::TextBox(const std::string &tString) : Button(tString)
     text.setCharacterSize(20);
     text.setStyle(0);
     setTextBoxSize(sf::Vector2f(0.f,0.f));
+    setTextPositionLeft();
+    setTexture(nullptr);
+    setFillColor(sf::Color::White);
+    setOutlineColor(sf::Color::Black);
+    setOutlineThickness(2);
 }
 
 TextBox::~TextBox()
@@ -23,7 +34,7 @@ void TextBox::setTextBoxSize(const sf::Vector2f &size)
 {
     //either max of text size or parameter size
     sf::FloatRect textBound = text.getGlobalBounds();
-    sf::Vector2f textSize = {textBound.width + 10, textBound.height + 10};
+    sf::Vector2f textSize = {textBound.width + 20, textBound.height + 20};
     textSize.x = std::max(textSize.x, size.x);
     textSize.y = std::max(textSize.y, size.y);
     setSize(textSize);
@@ -31,15 +42,20 @@ void TextBox::setTextBoxSize(const sf::Vector2f &size)
 
 void TextBox::setTextBoxPosition(const sf::Vector2f &size)
 {
-
+    setPosition(size);
+    setTextPositionLeft();
 }
 
 void TextBox::setTextBoxPosition(const float &x, const float &y)
 {
-
+    setPosition(x, y);
+    setTextPositionLeft();
 }
 
 void TextBox::setTextPositionLeft()
 {
-
+    sf::Vector2f rectanglePos = getPosition();
+    rectanglePos.x += 5;
+    rectanglePos.y += 5;
+    text.setPosition(rectanglePos);
 }
